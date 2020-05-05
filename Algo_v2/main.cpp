14d7bcda31ec235e22ef831c187c59cc5d09dba7
VirgileEcard <63054348+VirgileEcard@users.noreply.github.com>
2020-05-05 15:46:05 +0200

    Add files via upload

---
+++ Algo_v2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "algo.h"
+
+using namespace std;
+
+
+int main(int argc, char** argv){ //le programme requiert au moins un nom de fichier en argument
+
+  string nom1, nom2, seq1, seq2;
+
+  seqBasique(argv[1], &nom1, &nom2, &seq1, &seq2);
+
+  //on définit les pénalités (à faire : passer ça en arguments du main)
+  int pSup=1;
+  int pRemp=1;
+  // int distance;
+  
+  cout<<"En appliquant une pénalité de "<<pSup<<" pour une suppression et de "<<pRemp<<" pour un remplacement, ";
+  cout<<"la distance d'édition entre les séquences :"<<endl;
+  cout<<"-"<<nom1<<endl<<"-"<<nom2<<endl<<"est de : ";
+  distance(seq1, seq2, pSup, pRemp);
+  
+  return 0;
+}
